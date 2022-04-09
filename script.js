007cad53089da15b8e9bdc64c920a169a2a891ac
Nika Ko <97542937+Nikkakko@users.noreply.github.com>
2022-04-09 21:31:17 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const submitBtn = document.querySelector('.submit-btn');
+const thankYou = document.querySelector('.thankyou__state');
+const rating = document.querySelector('.rating-container');
+const ratingBtn = document.querySelectorAll('.rating-btn');
+const selected = document.querySelector('.selected');
+
+let voted = 0;
+
+submitBtn.addEventListener('click', () => {
+  if (thankYou.classList.contains('hidden')) {
+    rating.classList.add('hidden');
+    thankYou.classList.remove('hidden');
+  }
+});
+
+const amountOfSelected = function (voted) {};
+
+ratingBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.style.backgroundColor = 'hsl(25, 97%, 53%)';
+
+    voted = event.target.textContent;
+    console.log(voted);
+
+    selected.innerHTML = `
+      You selected ${voted} out of 5!
+    `;
+  });
+});
